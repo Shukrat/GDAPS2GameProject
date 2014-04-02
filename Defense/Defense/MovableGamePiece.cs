2027a7c2e93cffaa8b70e973679469bb3e0b8df4
Morrgan Sweeney-Charlton <mxs4952@rit.edu>
2014-04-02 19:06:48 -0400

    Main game and object code; move function does not work yet

---
+++ Defense/Defense/MovableGamePiece.cs
@@ -0,0 +1,49 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Defense
+{
+    // Morrgan Sweeney-Charlton; represents methods, attributes and properties common to objects placed on the game screen that can also move
+    // cannot be instantiated
+    abstract class MovableGamePiece:GamePiece
+    {
+        // attributes
+        protected int moveSpeed; // speed at which the piece moves across the screen
+
+        // properties
+        public int MoveSpeed
+        {
+            get { return moveSpeed; }
+        }
+
+        // parameterized contstructor
+        public MovableGamePiece(int hlt, int atk, int x, int y, int w, int h, String imgStr, int mvSp)
+            :base(hlt, atk, x, y, w, h, imgStr)
+        {
+            moveSpeed = mvSp;
+        }
+
+        // method stub for moving pieces on the screen
+        // will add more once I get a feel for what it needs to do in its entirety
+        // is virtual so that child classes can override it as needed
+        public virtual void Move()
+        {
+        }
+
+        // method stub for moving pieces on the screen
+        // will add more once I get a feel for what it needs to do in its entirety
+        // is virtual so that child classes can override it as needed
+        // overloaded so certain classes can use it
+        public virtual void Move(Enemy en)
+        {
+        }
+    }
+}
---
+++ Defense/Defense/Projectile.cs
@@ -0,0 +1,55 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+namespace Defense
+{
+    // Morrgan Sweeney-Charlton; class for handling any and everything projectiles can do
+    class Projectile:MovableGamePiece
+    {
+        // attributes
+        string effect; // string representing status ailments projectile inflicts
+        bool active; // boolean determining if the projectile is in use
+
+        // properties
+        public string Effect
+        {
+            get { return effect; }
+        }
+        public bool Active
+        {
+            get { return active; }
+            set { active = value; }
+        }
+
+        // parametrized constructor
+        public Projectile(int hlt, int atk, int x, int y, int w, int h, String imgStr, int mvSp, string eft)
+            : base(hlt, atk, x, y, w, h, imgStr, mvSp)
+        {
+            effect = eft;
+            active = false;
+        }
+
+        // move the projectile toward the enemy
+        public override void Move(Enemy en)
+        {
+            /*
+            while (active == true)
+            {
+                this.pieceShape.X = this.pieceShape.X + (this.moveSpeed - en.PieceShape.X);
+                this.pieceShape.Y = this.pieceShape.Y + (this.moveSpeed - en.PieceShape.Y);
+                if (this.pieceShape.Intersects(en.PieceShape) == true)
+                {
+                    active = false;
+                }
+            }
+              */
+        }
+    }
+}
